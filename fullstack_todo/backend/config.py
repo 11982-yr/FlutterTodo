23fcefa40b40ee1426c10b32e4aad9337f6948a7
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-04-12 17:15:17 +0300

    Create config.py
    config.py: a file connect flask app to the database.

---
+++ fullstack_todo/backend/config.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+"""
+this file connect flask app to the database.
+"""
+
+load_dotenv() #this line loads the .env file.
+
+
+class Config:
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PORT = os.getenv("DB_PORT")
+
+    # the line here build the database connection URL.
+    """
+    since we use PostgreSQL database this will Connect to a PostgreSQL
+    database running on localhost, port 5432, with user postgres,
+    password postgres123, and use the database todo_app.
+    """
+    SQLALCHEMY_DATABASE_URI = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
